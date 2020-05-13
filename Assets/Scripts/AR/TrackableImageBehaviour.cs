2b9a857cdac63851f743e5235c51698a20fa9b80
Александр <cahbka13@gmail.com>
2020-05-13 21:28:14 +0300

    Update TrackableImageBehaviour.cs

--- Assets/Scripts/AR/TrackableImageBehaviour.cs
+++ Assets/Scripts/AR/TrackableImageBehaviour.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor.SceneManagement;
+﻿#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.ARFoundation;
