cd91f6c4c5c723baea6466fbf5f1b26c03e78345
Alex <rariaden@gmail.com>
2020-05-19 21:44:29 +0300

    Added selection blink

---
+++ Assets/Scripts/SelectionBlink.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AssetVariables;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SelectionBlink : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private FloatVariable blinkTime;
+    [SerializeField] private FloatVariable blinkDelay;
+
+    [Header("References")]
+    [SerializeField] private MeshRenderer selectionRenderer;
+    
+    private float timer;
+    private bool isBlinking;
+
+    
+
+    private void OnEnable()
+    {
+        timer = 0;
+        isBlinking = false;
+        selectionRenderer.enabled = true;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (isBlinking)
+        {
+            if (timer < blinkTime)
+                return;
+            
+            timer = 0;
+            isBlinking = false;
+            selectionRenderer.enabled = true;
+        }
+        else
+        {
+            if (timer < blinkDelay)
+                return;
+            
+            timer = 0;
+            isBlinking = true;
+            selectionRenderer.enabled = false;
+        }
+    }
+}
