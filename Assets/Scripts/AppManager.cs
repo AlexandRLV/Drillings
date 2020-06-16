90ff5ecfb5baee87b74a4213d2c21b5a94db1dbb
Alex <rariaden@gmail.com>
2020-06-16 17:56:00 +0300

    Moved objects

--- Assets/Scripts/AppManager.cs
+++ Assets/Scripts/AppManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Data;
 using UI;
-using UnityEngine.XR.ARFoundation;
 
 public class AppManager : MonoBehaviour
 {
@@ -14,7 +13,6 @@ public class AppManager : MonoBehaviour
     [Header("References")]
     [SerializeField] private UIManager uiManager;
     [SerializeField] private Compass compass;
-    [SerializeField] private ARSession arSession;
 
     [Header("Layouts")]
     [SerializeField] private List<LayoutDataContainer> scenesLayoutDataAssets;
@@ -40,7 +38,7 @@ public class AppManager : MonoBehaviour
 	    Debug.Log($"AppManager: Activating {objectName}");
 	    if (CurrentLayout != null || !string.IsNullOrWhiteSpace(CurrentObjectName))
 	    {
-		    Debug.Log($"AppManager: Already activated {CurrentObjectName}, returning");
+		    Debug.Log($"AppManager: Already activated object, returning");
 		    return;
 	    }
 	    
@@ -81,8 +79,6 @@ public class AppManager : MonoBehaviour
         CurrentObjectName = null;
         objectTransform = null;
         Debug.Log("AppManager: Deactivated");
-        
-        arSession.Reset();
 
         return true;
     }
