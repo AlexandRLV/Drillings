a235af060e4c7d72047f62f4359ab36ec8fa409c
RariAden (Work) <rariaden@gmail.com>
2020-07-02 16:50:50 +0300

    Added new scans and root offsets

--- Assets/Scripts/AR/Trackable.cs
+++ Assets/Scripts/AR/Trackable.cs
@@ -6,7 +6,7 @@ namespace AR
     [Serializable]
     public class Trackable
     {
-        public bool IsActive => targetObject.activeSelf;
+        public bool IsActive { get; private set; }
         public bool IsFound => objectTransform != null;
         public string ReferenceName => referenceName;
         public Vector3 Position => TargetTransform.position;
@@ -23,6 +23,8 @@ namespace AR
     
         [SerializeField] private string referenceName;
         [SerializeField] private GameObject targetObject;
+        [SerializeField] private Vector3 objectRootPosition;
+        [SerializeField] private Vector3 objectRootRotation;
         
         private Transform TargetTransform
         {
@@ -34,33 +36,51 @@ namespace AR
                 return targetTransform;
             }
         }
-        private Transform targetTransform;
+        private Transform ChildTransform
+        {
+            get
+            {
+                if (childTransform == null)
+                    childTransform = TargetTransform.GetChild(0);
+
+                return childTransform;
+            }
+        }
 
         private LayoutController layoutController;
         private Transform objectTransform;
+        private Transform targetTransform;
+        private Transform childTransform;
         
 
 
         public void UpdateObjectTransform(Transform transform)
         {
             objectTransform = transform;
-            UpdatePositionAndRotation();
+            
+            if (IsActive)
+                UpdatePositionAndRotation();
         }
         
         public void UpdatePositionAndRotation()
         {
             TargetTransform.position = objectTransform.position;
             TargetTransform.rotation = Quaternion.Euler(0, objectTransform.rotation.eulerAngles.y, 0);
+
+            ChildTransform.localPosition = objectRootPosition;
+            ChildTransform.localRotation = Quaternion.Euler(objectRootRotation);
         }
 
         public void Enable()
         {
             targetObject.SetActive(true);
+            IsActive = true;
         }
 
         public void Disable()
         {
             targetObject.SetActive(false);
+            IsActive = false;
         }
     }
 }
